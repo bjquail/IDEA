207745f57180bc852b8b74038a5283f631587c92
Ben Quail <bjquail@gmail.com>
2025-04-12 23:11:32 +0100

    Add homepage to load chat UI

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import IdeaChat from "../App";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>IDEA – Instructional Design Experience Assistant</title>
+      </Head>
+      <IdeaChat />
+    </>
+  );
+}
